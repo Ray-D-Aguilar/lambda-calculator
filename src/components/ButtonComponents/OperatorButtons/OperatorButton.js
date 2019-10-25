82ac8c7bbeefc9e49cb169185ee06559b0f119e2
Ray Aguilar <ray.aguilar15@gmail.com>
2019-10-24 19:50:01 -0500

    slowly figuring out what i need to do, operatorbutton and numberbutton are update

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const OperatorButton = () => {
+export const OperatorButton = props => {
+  console.log(props.operator.char);
   return (
-    <>
+    <button className="operatorButton">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.operator.char}
+    </button>
   );
 };
